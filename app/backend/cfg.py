e1d59b1fb4f867e0d4d7d32974f36cb37176e0dc
Vulcan_Chen <1633080461@qq.com>
2023-11-04 15:38:16 +0800

    style:去除敏感信息

--- app/backend/cfg.py
+++ app/backend/cfg.py
@@ -1,9 +1,5 @@
 database_cfg = {
-    'host': '39.108.37.159',
-    'port': 27017,
-    'username': 'admin',
-    'password': 'coder-2023',
-    'authSource': 'admin',
+
 }
 
 run_cfg = {
@@ -14,6 +10,5 @@ run_cfg = {
 
 # sp23的信息
 wx_app_cfg = {
-    'appid': 'wx2fdfc27744ffa252', # 2023-10-20
-    'secret': '9da819bd0531b325f5ebda4f85a92503' # 2023-10-20 更新
+
 }
